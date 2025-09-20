387b245116d63ed398ca2ce942c42cd4431aafbf
Henrietta Gabriel <henridivine@gmail.com>
2025-09-20 11:11:56 -0600

    Implemented remaining ChessBoard functionality - overriding equals() and hashCode(), adding functions for remove one or all pieces, implementing resetBoard() and setting all the pieces in their correct places

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.*;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -11,9 +13,7 @@ public class ChessBoard {
     // create a double array of chess pieces, with 8 rows and 8 columns
     private final ChessPiece[][] board = new ChessPiece[8][8];
 
-    public ChessBoard() {
-        
-    }
+    public ChessBoard() { }
 
     /**
      * Adds a chess piece to the chessboard
@@ -25,6 +25,8 @@ public class ChessBoard {
         // assign the given piece to the given position; "this position should hold this piece"
         // remember that the given position is 1-based, so we have to subtract 1 to get the index to access
         board[position.getRow() - 1][position.getColumn() - 1] = piece;
+
+        // my note: remember that board is a double array of ChessPieces
     }
 
     /**
@@ -39,11 +41,84 @@ public class ChessBoard {
         return board[position.getRow() - 1][position.getColumn() - 1];
     }
 
+    public void removePiece(ChessPosition position) {
+        board[position.getRow() - 1][position.getColumn() - 1] = null;
+    }
+
+    public void removeAllPieces() {
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                board[r][c] = null;
+            }
+        }
+    }
+
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+
+        removeAllPieces();
+
+        // add non-pawn white pieces (row 1)
+        setOtherPieces(1, ChessGame.TeamColor.WHITE);
+
+        // add white pawns
+        for (int c = 1; c <= 8; c++) {
+            ChessPosition piecePosition = new ChessPosition(2, c);
+            ChessPiece piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            addPiece(piecePosition, piece);
+        }
+
+        // add black pawns
+        for (int c = 1; c <= 8; c++) {
+            ChessPosition piecePosition = new ChessPosition(7, c);
+            ChessPiece piece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+            addPiece(piecePosition, piece);
+        }
+
+        // add non-pawn black pieces (row 8)
+        setOtherPieces(8, ChessGame.TeamColor.BLACK);
+
+    }
+
+    private void setOtherPieces(int r, ChessGame.TeamColor color) {
+
+        int c = 1;
+
+        List<ChessPiece.PieceType> typesInOrder = List.of(
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK
+        );
+
+        for (ChessPiece.PieceType pieceType : typesInOrder) {      // for each of the chess piece types in order...
+        if (pieceType != ChessPiece.PieceType.PAWN) {                           // if it's not a pawn...
+            ChessPosition piecePosition = new ChessPosition(r, c);              // make a chess position with the given row and a column starting from 1
+            ChessPiece piece = new ChessPiece(color, pieceType);                // make a chess piece with the given color and the current piece type
+            addPiece(piecePosition, piece);                                     // add a piece with those details
+            c++;                                                                // go to next column
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
     }
 }
